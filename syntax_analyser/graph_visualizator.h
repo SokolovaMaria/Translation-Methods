ae22d1f82d0ae4be3b2369027bf30faba6bf4c15
Auslender <marysok17@gmail.com>
2017-11-15 10:43:38 +0300

    2lab

--- syntax_analyser/graph_visualizator.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYNTAX_ANALYSER_TREE_CREATOR_H
-#define SYNTAX_ANALYSER_TREE_CREATOR_H
-
-#include <cstdio>
-#include "pascal_array_declaration/parser.h"
-
-FILE* createPNG(std::string, parser::graph_iterator&);
-
-#endif //SYNTAX_ANALYSER_TREE_CREATOR_H
--- syntax_analyser/main.cpp
+++ syntax_analyser/main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 #include "pascal_array_declaration/parser.h"
-#include "graph_visualizator.h"
+#include <graphviz/gvc.h>
 
+static GVC_t* gvc;
+static Agraph_t *G;
+static int counter;
+
+std::string get_fresh_label() {
+    counter++;
+    return std::to_string(counter);
+}
+
+Agnode_t* createGraph(Agraph_t* G, parser::graph_iterator &it) {
+    Agnode_t* parent_node = agnode(G, &get_fresh_label()[0], 1);
+    std::string attr_name = "label";
+    agset(parent_node, &attr_name[0], &it.get_str()[0]);
+    for (auto& child : it.get_children()) {
+        Agnode_t* child_node = createGraph(G, child);
+        agedge(G, parent_node, child_node, nullptr, 1);
+    }
+    return parent_node;
+}
+
+FILE* createPNG(std::string file_name, parser::graph_iterator& it) {
+    gvc = gvContext();
+    std::string g_name = "graph";
+    G = agopen(&g_name[0], Agstrictdirected, 0);
+
+    counter = 0;
+    createGraph(G, it);
+
+    gvLayout(gvc, G, "dot");
+    FILE* png = fopen(file_name.c_str(), "w");
+    gvRender(gvc, G, "png", png);
+    gvFreeLayout(gvc, G);
+
+    agclose(G);
+    assert(gvFreeContext(gvc) == 0);
+    return png;
+}
 
 int main() {
     parser p;
@@ -20,5 +57,4 @@ int main() {
         std::cerr << e.what() << std::endl;
         std::cerr << "Wrong array declaration\n";
     }
-
 }
